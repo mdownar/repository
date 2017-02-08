dc9ed3f0002ca32d9024981d1e1455f9a9de916d
mdownar <mdownar@CE14095.corp.capgemini.com>
2017-02-08 08:58:36 +0100

    Initial project setup

---
+++ Fibonacci/src/com/capgemini/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package com.capgemini.fibonacci;
+
+/**
+ * TODO add javadoc
+ * 
+ * @author mdownar
+ *
+ */
+public class Fibonacci {
+	
+}
---
+++ Fibonacci/test/com/capgemini/fibonacci/FibonacciTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.fibonacci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
